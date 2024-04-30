b3661c16b22ffbab3e3ca3426dbe6965daeb804e
Soham Sarkar <sohamsarkar59@gmail.com>
2024-04-30 15:23:54 +0530

    initial

---
+++ src/popup.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+    const numbers = document.querySelector("#number").value.split(",");
+    const message = document.querySelector("#message").value;
+});
