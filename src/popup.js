fc15f2ae9621f66cec1548abe4f074e9f20f5013
Soham Sarkar <sohamsarkar59@gmail.com>
2024-05-01 15:04:28 +0530

    added a messaging system for sending accquired data to content scripts

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,24 @@
-const btn = document.querySelector("#btn");
+document.addEventListener("DOMContentLoaded", function () {
+    chrome.tabs.query({ url: "https://web.whatsapp.com/*" }, function (tabs) {
+        if (tabs.length === 0) {
+            chrome.tabs.create({ url: "https://web.whatsapp.com/" });
+        }
+    });
+});
 
-btn.addEventListener("click", () => {
+const button = document.querySelector("#btn");
+
+button.addEventListener("click", async () => {
     const numbers = document.querySelector("#number").value.split(",");
     const message = document.querySelector("#message").value;
+
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+        const response = await chrome.tabs.sendMessage(tabs[0].id, {
+            action: "send_message",
+            numbers,
+            message,
+        });
+
+        console.log(response);
+    });
 });
