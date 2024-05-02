bedbe9c5321904935ea48ee498fc3d25d71e8191
Soham Sarkar <sohamsarkar@Sohams-MacBook-Air.local>
2024-05-02 20:13:17 +0530

    sorry

--- content.js
+++ content.js
@@ -1,14 +1,45 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "send_message") {
-        const { numbers, message } = request;
+const findElement = (selector) => document.querySelector(selector);
 
-        numbers.forEach((number) => {
-            const url = `https://web.whatsapp.com/send/?phone=${number}&text=${encodeURIComponent(message)}&type=phone_number&app_absent=1`;
+const awaitFor = (selector) => {
+  console.log("I have been called");
+  return new Promise((resolve, reject) => {
+    const readyToSend = findElement(selector);
 
-            window.location.href = url;
-        });
+    const invalid = readyToSend
+      ? readyToSend.ariaLabel.includes("Voice message")
+      : false;
 
-        // Send a response back to the popup script
-        sendResponse({ received: true });
+    if (!invalid) {
+      resolve();
     }
+    setTimeout(() => {
+      awaitFor(selector).then(resolve);
+    }, 100);
+  });
+};
+
+async function clickToSend() {
+  try {
+    await awaitFor("[data-tab='11']");
+    document.querySelectorAll("footer button")[4].click();
+  } catch (e) {
+    console.log("Error: ", e);
+  }
+}
+
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  if (request.action === "send_message") {
+    const { numbers, message } = request;
+
+    await clickToSend();
+
+    for (let i = 0; i < numbers.length; i++) {
+      const url = `https://web.whatsapp.com/send/?phone=${numbers[i]}&text=${encodeURIComponent(message)}&type=phone_number`;
+
+      window.location.href = url;
+    }
+
+    // Send a response back to the popup script
+    sendResponse({ received: true });
+  }
 });
--- src/popup.js
+++ src/popup.js
@@ -1,24 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    chrome.tabs.query({ url: "https://web.whatsapp.com/*" }, function (tabs) {
-        if (tabs.length === 0) {
-            chrome.tabs.create({ url: "https://web.whatsapp.com/" });
-        }
-    });
+  chrome.tabs.query({ url: "https://web.whatsapp.com/*" }, function (tabs) {
+    if (tabs.length === 0) {
+      chrome.tabs.create({ url: "https://web.whatsapp.com/" });
+    }
+  });
 });
 
 const button = document.querySelector("#btn");
 
 button.addEventListener("click", async () => {
-    const numbers = document.querySelector("#number").value.split(",");
-    const message = document.querySelector("#message").value;
+  const numbers = document.querySelector("#number").value.split(",");
+  const message = document.querySelector("#message").value;
 
-    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-        const response = await chrome.tabs.sendMessage(tabs[0].id, {
-            action: "send_message",
-            numbers,
-            message,
-        });
-
-        console.log(response);
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    console.log(tabs);
+    const response = await chrome.tabs.sendMessage(tabs[0].id, {
+      action: "send_message",
+      numbers,
+      message,
     });
+
+    console.log(response);
+  });
 });
